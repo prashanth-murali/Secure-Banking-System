022fd16809d79b1ccbbf41dfb7efea41df808c9a
James Kieley <James.Kieley@lifelock.com>
2017-10-02 17:08:21 -0700

    wire up router

--- shared/front-end/my_app/src/app/app.js
+++ shared/front-end/my_app/src/app/app.js
@@ -1,23 +1,45 @@
 import angular from 'angular';
+import '@uirouter/angularjs';
 
 import '../style/app.css';
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
-
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, []).directive('app', app).controller('AppCtrl', AppCtrl);
+let app = angular.module(MODULE_NAME, ['ui.router']);
+
+app.directive('app', function(){
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
+});
+
+app.controller('AppCtrl', function(){
+    this.url = 'https://github.com/preboot/angular-webpack';
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '../views/login_page.html'
+        })
+
+        .state('dashboard_admin', {
+            url: '/dashboard_admin',
+            templateUrl: '../views/internal_users/dashboard_admin.html'
+        })
+
+        .state('about', {
+            // we'll get to this in a bit
+        });
+
+});
+
 
 export default MODULE_NAME;

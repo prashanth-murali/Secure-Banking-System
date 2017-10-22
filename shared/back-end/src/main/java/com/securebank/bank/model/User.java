b42694e3041fac177d07a31d587fa6bdda716536
James Kieley <James.Kieley@lifelock.com>
2017-10-21 19:31:27 -0700

    add email getter setter

--- shared/back-end/src/main/java/com/securebank/bank/model/User.java
+++ shared/back-end/src/main/java/com/securebank/bank/model/User.java
@@ -100,6 +100,14 @@ public class User {
         this.accounts = accounts;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "User{" +

28d9fcd83420302005fca99ed163ae3184f3b783
o4siang <bob89069@gmail.com>
2017-10-24 11:09:38 -0700

    bug fixed

--- shared/back-end/src/main/java/com/securebank/bank/resources/LoginResource.java
+++ shared/back-end/src/main/java/com/securebank/bank/resources/LoginResource.java
@@ -5,12 +5,22 @@ import com.securebank.bank.model.User;
 import com.securebank.bank.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import javax.ws.rs.core.Response;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import com.securebank.bank.services.LoggedInService;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.*;
+import java.util.Date;
 import javax.ws.rs.core.MediaType;
 
 @Component

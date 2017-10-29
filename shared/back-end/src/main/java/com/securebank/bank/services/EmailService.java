2456dc97ddbe35c2321ce388b27174f4d588de63
James Kieley <James.Kieley@lifelock.com>
2017-10-29 16:58:55 -0700

    use gmail instead

--- shared/back-end/src/main/java/com/securebank/bank/services/EmailService.java
+++ shared/back-end/src/main/java/com/securebank/bank/services/EmailService.java
@@ -1,19 +1,13 @@
 package com.securebank.bank.services;
 
 import com.securebank.bank.resources.TransactionsResource;
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class EmailService {
@@ -22,23 +16,18 @@ public class EmailService {
     private @Value("${sendgrid-api-key}") String sendGridKey;
 
     public void sendEmail(String toEmailAddress, String emailMessageBody){
-        Email from = new Email("fromgroup5@softwaresecurity.xyz");
-        String subject = "Bank-Service-Group5";
-        Email to = new Email(toEmailAddress);
-        Content content = new Content("text/plain", emailMessageBody);
-        Mail mail = new Mail(from, subject, to, content);
-
-        SendGrid sg = new SendGrid(sendGridKey);
-        Request request = new Request();
         try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            logger.info(String.valueOf(response.getStatusCode()));
-            logger.info(response.getBody());
-            logger.info(response.getHeaders().toString());
-        } catch (IOException ex) {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("cse545g5@gmail.com", "8xOSl6jwsNQ~64AOLkHAcgUwR"));
+            email.setSSLOnConnect(true);
+            email.setFrom("user@gmail.com");
+            email.setSubject("TestMail");
+            email.setMsg(emailMessageBody);
+            email.addTo(toEmailAddress);
+            email.send();
+        } catch (Exception ex) {
             logger.error("error sending email", ex);
         }
     }
